b3d9036888c449b50eb1f6374d8513f392e34ca0
ktlo <ktlo@handtruth.com>
2025-05-11 02:21:38 +0300

    Kotlin build scripts

--- maven.gradle.kts
+++ maven.gradle.kts
@@ -3,8 +3,8 @@ repositories {
     maven(url = "https://maven.pkg.github.com/handtruth/maven") {
         name = "HandTruth"
         credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
         }
     }
     google()
